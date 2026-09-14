c9803decba64b2956a95cdc827fdabbb28a7fb31
Wazida-in-code <wazidamomtazesha@gmail.com>
2026-09-14 23:10:07 +0600

    Added:Navbar

---
+++ src/app/Updates/Developers/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Developers = () => {
+    return (
+        <div>
+            <h2>I am a new developer!</h2>
+        </div>
+    );
+};
+
+export default Developers;
