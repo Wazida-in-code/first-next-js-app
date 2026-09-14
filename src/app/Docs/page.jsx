e5009afd3266469d54af9316ae864d9be622d0c0
Wazida-in-code <wazidamomtazesha@gmail.com>
2026-09-14 21:45:08 +0600

    first commit

---
+++ src/app/Docs/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DocsPage = () => {
+    return (
+        <div>
+            <h1>My First Documentation!</h1>
+        </div>
+    );
+};
+
+export default DocsPage;
